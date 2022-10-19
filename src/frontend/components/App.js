c16e574b4ffd29d4e08ba187abd3a539055eac5e
SapphireHotS <bogdan_gabor2020@hotmail.com>
2022-10-19 15:29:12 +0400

    first commit

---
+++ src/frontend/components/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+ 
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 ms-3"
+          href="https://www.conquerblocks.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Conquer Blocks
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mx-auto mt-5">
+              <a
+                href="http://www.conquerblocks.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              </a>
+              <h1 className= "mt-5">Marketplace Starter Kit</h1>
+              <p>
+                Edit <code>src/frontend/components/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="http://www.conquerblocks.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+              </a>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
